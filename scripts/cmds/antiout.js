3d0bb5972390f690c80196392affa85ece1c92d4
Nagraj323 <jfkbdkfkfbflnfjj@gmail.com>
2026-09-11 15:20:33 +0600

    Update antiout.js

--- scripts/cmds/antiout.js
+++ scripts/cmds/antiout.js
@@ -1,3 +1,4 @@
+const fs = require("fs-extra"); 
 module.exports = {
  config: {
  name: "antiout",
@@ -19,8 +20,8 @@ module.exports = {
 
  langs: {
  en: {
- turnedOn: "🛡️ Anti-out feature has been enabled for this group",
- turnedOff: "🛡️ Anti-out feature has been disabled for this group",
+ turnedOn: "✅ Anti-out on",
+ turnedOff: "❎ Anti-out off",
  missingPermission: "❌ Sorry boss! I couldn't add the user back.\nUser %1 might have blocked me or doesn't have messenger option enabled.",
  addedBack: "⚠️ Attention %1!\nThis group belongs to my boss!\nYou need admin clearance to leave this group!"
  }
